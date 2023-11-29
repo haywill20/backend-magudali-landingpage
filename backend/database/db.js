b67452446c3a7b58fde6a7e87e3a3c4c578fb493
haywill20 <mirandacaceresm6@gmail.com>
2023-11-28 21:19:28 -0600

    se reemplazó el nombre de la base de datos

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("ladingpage_db", "root", "", {
+const db = new Sequelize("landingpage_db", "root", "", {
   host: "localhost",
   dialect: "mysql",
 });
