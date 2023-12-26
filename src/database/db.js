7dbf59eef0a60c02de27a77cafde1163e458ee5a
haywill20 <mirandacaceresm6@gmail.com>
2023-12-26 13:38:31 -0600

    se agregaron los campos correspondientes a usuario y contraseña de la base de datos

--- src/database/db.js
+++ src/database/db.js
@@ -1,8 +1,13 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("maguinev_landingpage_db", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-});
+const db = new Sequelize(
+  "maguinev_landingpage_db",
+  "maguinev_magudali",
+  "magudali@21",
+  {
+    host: "localhost",
+    dialect: "mysql",
+  }
+);
 
 export default db;
