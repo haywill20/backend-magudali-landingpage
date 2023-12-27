aa1316ac4692b7004c66cb3844d82cd53da83570
haywill20 <mirandacaceresm6@gmail.com>
2023-12-26 22:23:44 -0600

    agregando funcion async a el archivo app

--- src/app.js
+++ src/app.js
@@ -12,19 +12,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", routes);
-
-try {
-  await db.authenticate();
-  console.log("Conexion exitosa a la base de datos");
-} catch (error) {
-  console.log(`El error de conexion es: ${error}`);
-}
-
-/* app.get("/", (req, res) => {
+async function startServer() {
+  try {
+    await db.authenticate();
+    console.log("Conexion exitosa a la base de datos");
+    /* app.get("/", (req, res) => {
   res.send("Hola Mundo");
 }); */
 
-app.use("/images", express.static("./uploads/comercios"));
-app.listen(8000, () => {
-  console.log("Server UP running in http://localhost:8000/");
-});
+    app.use("/images", express.static("./uploads/comercios"));
+    app.listen(8000, () => {
+      console.log("Server UP running in http://localhost:8000/");
+    });
+  } catch (error) {
+    console.log(`El error de conexion es: ${error}`);
+  }
+}
+
+startServer(); // Llama a la función para iniciar el servidor
