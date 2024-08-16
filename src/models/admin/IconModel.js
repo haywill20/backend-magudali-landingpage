6e4ed1fbbdfdb40b3c320d0b463d3566d55a4dcb
haywill20 <mirandacaceresm6@gmail.com>
2024-08-15 21:15:21 -0600

    refactor: Se renombraron los campos y el nombre de la tabla iconos a ingles

--- src/models/admin/IconModel.js
+++ src/models/admin/IconModel.js
@@ -2,9 +2,9 @@
 import db from "../../database/db.js";
 import { DataTypes } from "sequelize";
 
-const IconModel = db.define("iconos", {
-  nombre: { type: DataTypes.STRING, required: true },
-  valor: { type: DataTypes.STRING, required: true }
+const IconModel = db.define("icons", {
+  name: { type: DataTypes.STRING, required: true },
+  code: { type: DataTypes.STRING, required: true },
 });
 
 export default IconModel;
